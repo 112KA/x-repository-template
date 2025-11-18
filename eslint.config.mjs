8ee4040865f823044618d2d32a25a8382941c7f8
iizuka <iizuka@1-10.com>
2025-11-18 10:03:41 +0900

    Squashed 'packages/x3/' changes from 8bfcb25..6f3ee41
    6f3ee41 fix: update stats-gl dependency to version 3.7.0 in package.json and pnpm-lock.yaml
    35741c9 refactor: enhance type annotations and improve code clarity
    bc34c4b Merge commit 'f968815a0a916c1cb1156c260880e77cdd9c9b4d' into main
    88ee144 Refactor code structure for improved readability and maintainability
    bffdf0a Refactor code structure for improved readability and maintainability
    90b5267 Merge commit '19506b171256d1472e75d21a982608e9b5fc2e55' into main
    5f1f7ea Merge commit '422f5deba8b66e541bf4fece12fdc4338e840c26' into main
    ab0fbbc Merge commit 'ceb427d963a92bf29c5dcfded35b89234bb61277' as 'packages/x3/src'
    5cda114 remove local packages/x3/src to replace with x3-src
    
    git-subtree-dir: packages/x3
    git-subtree-split: 6f3ee4134a1b115152d7d7875fee9904227ffd63

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,12 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu(
   {
+    type: 'lib',
+
     ignores: ['**/*.d.ts', 'src/@types/**'],
+
+    typescript: true,
+
+    stylistic: true,
   },
 )
